facdf873d301bce433e0c4db5b872425b6477272
DJ Sandra <sandra.kriemann@zanox.com>
2015-07-27 12:56:41 +0200

    fix wrong import

--- src/main/java/verticles/MongoDBVerticle.java
+++ src/main/java/verticles/MongoDBVerticle.java
@@ -1,7 +1,7 @@
 package verticles;
 
 import io.vertx.core.json.JsonObject;
-import com.mongodb.async.client.MongoClient;
+import io.vertx.ext.mongo.MongoClient;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.MessageConsumer;
@@ -17,13 +17,11 @@ public class MongoDBVerticle extends AbstractVerticle{
 
     @Override
     public void start() {
-        final MongoClient mongo = MongoClient.createShared(vertx, new JsonObject().put("db_name", "demo"));
+        final MongoClient mongo = MongoClient.createNonShared(vertx, new JsonObject().put("db_name", "demo"));
 
         EventBus eb = vertx.eventBus();
         MessageConsumer<String> consumer = eb.consumer(MongoDBVerticle.class.getSimpleName());
-        consumer.handler(message -> {
-            writeToMongoDB();
-        });
+        consumer.handler(message -> writeToMongoDB());
     }
 
     private void writeToMongoDB() {
