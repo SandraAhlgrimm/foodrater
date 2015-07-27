46e259c4582f268d3871ff6226b9c94fd74c568d
DJ Sandra <sandra.kriemann@zanox.com>
2015-07-27 16:09:15 +0200

    tried fatjar

---
+++ src/main/scripts/foodrater.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#
+# foodrater	Startup script for foodrater
+#
+SCRIPTDIR=$(dirname "$0")
+BASEDIR=$(dirname "$SCRIPTDIR")
+
+JAVA_OPTS="-server -Xmx1G -XX:NewRatio=50 -XX:+HeapDumpOnOutOfMemoryError -XX:+UseG1GC"
+JAVA_OPTS="$JAVA_OPTS -XX:HeapDumpPath=/srv/headpdumps"
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=5566"
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.local.only=false -Dcom.sun.management.jmxremote.authenticate=false"
+JAVA_OPTS="$JAVA_OPTS -Dcom.sun.management.jmxremote.ssl=false"
+
+JAR_FILE="$BASEDIR/foodrater-*-fat.jar"
+
+function start() {
+    echo "Starting JAR_FILE"
+    START_SCRIPT="/usr/bin/java $JAVA_OPTS -jar $JAR_FILE"
+    echo $START_SCRIPT
+    exec $START_SCRIPT
+}
+
+start
